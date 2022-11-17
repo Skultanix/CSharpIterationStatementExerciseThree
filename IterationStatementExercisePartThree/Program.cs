bb161a08ea97b7dacd418442147923a3a2f51ce1
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-11-17 16:06:30 -0700

    Part three completed.

--- IterationStatementExercisePartThree/Program.cs
+++ IterationStatementExercisePartThree/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a number for which you wish to see factorialized (presuming that's a real term).");
+
+            var userInput = Console.ReadLine();
+            int theNumberInQuestion = int.Parse(userInput);
+
+            for (var i = theNumberInQuestion - 1; i > 0; i--)
+            {
+                theNumberInQuestion = i * theNumberInQuestion;
+            }
+
+            Console.WriteLine(string.Format("The factorial of {0} is {1}.", userInput, theNumberInQuestion));
         }
     }
 }
